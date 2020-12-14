3518dbd8049e4cd3b896c2969b73b2f29394892a
sunxiongfei <sunxiongfei@xiaomi.com>
2020-12-14 15:38:59 +0800

    路径总和递归&广度优先搜索

--- 112.路径总和.go
+++ 112.路径总和.go
@@ -13,7 +13,36 @@
  *     Right *TreeNode
  * }
  */
-// func hasPathSum(root *TreeNode, sum int) bool {
+
+// 广度优先搜索
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	nq, vq := []*TreeNode{root}, []int{root.Val}
+	for len(nq) > 0 {
+		node := nq[0]
+		nq = nq[1:]
+		val := vq[0]
+		vq = vq[1:]
+		if node.Left == nil && node.Right == nil && val == sum {
+			return true
+		}
+		if node.Left != nil {
+			nq = append(nq, node.Left)
+			vq = append(vq, val + node.Left.Val)
+		}
+		if node.Right != nil {
+			nq = append(nq, node.Right)
+			vq = append(vq, val + node.Right.Val)
+		}
+	}
+	return false
+}
+// @lc code=end
+
+// 递归
+// func recursiveHasPathSum(root *TreeNode, sum int) bool {
 // 	if root == nil {
 // 		return false
 // 	}
@@ -22,15 +51,4 @@
 // 	}
 // 	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
 // }
-// @lc code=end
-
-func hasPathSum(root *TreeNode, sum int) bool {
-	if root == nil {
-		return false
-	}
-	if root.Left == nil && root.Right == nil {
-		return sum == root.Val
-	}
-	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
-}
-// @lc code=end
+//
