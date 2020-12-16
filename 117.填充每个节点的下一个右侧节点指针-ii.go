317fa1243ff9436634f88ba86a7d552478b737e1
sunxiongfei <sunxiongfei@xiaomi.com>
2020-12-16 11:26:28 +0800

    b tree

---
+++ 117.填充每个节点的下一个右侧节点指针-ii.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=golang
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+ func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	q := []*Node{root}
+	for len(q) > 0 {
+		tmp := q
+		q = nil
+		for i, node := range tmp {
+			if i+1 < len(tmp) {
+				node.Next = tmp[i+1]
+			}
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+	}
+	return root
+}
+// @lc code=end
+
